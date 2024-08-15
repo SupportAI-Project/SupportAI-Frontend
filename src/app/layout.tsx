55bcaf0214c263077d405570299f316e9eb4728b
nitzanto <nitzan16001@gmail.com>
2024-08-15 17:01:12 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body style={{ height: "100vh" }} className={inter.className}>
-        
-        <Providers>{children}
-      </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
