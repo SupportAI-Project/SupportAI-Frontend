85d2513af16a7539c9d3d7040021fdd870049af7
Roie <roiy0987@gmail.com>
2024-08-18 21:50:30 +0300

    refactor: Update socket URL handling to use environment variable

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,18 @@
 import { io, Socket } from "socket.io-client";
+import { z } from "zod";
 
-let socket: Socket = io("http://localhost:3001/chat", {
+const socketUrlSchema = z.string().url();
+
+const socketUrlEnv = process.env.NEXT_PUBLIC_SOCKET_URL;
+const socketUrl = socketUrlSchema.safeParse(socketUrlEnv);
+
+if (!socketUrl.success) {
+  throw new Error(
+    `Invalid SOCKET_URL environment variable: ${socketUrl.error.message}`
+  );
+}
+
+let socket: Socket = io(socketUrl.data, {
   transports: ["websocket", "polling"],
   autoConnect: false,
 });
