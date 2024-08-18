c7a872bcfca05600aaf0c1d0bcefc7dabdf4c955
Roie <roiy0987@gmail.com>
2024-08-18 20:30:45 +0300

    refactor: update MessageInput component with note and reply functionality

--- src/app/(admin)/new_dashboard/components/MessageInput/index.tsx
+++ src/app/(admin)/new_dashboard/components/MessageInput/index.tsx
@@ -1,4 +1,10 @@
-import { Box, TextField, IconButton } from "@mui/material";
+import {
+  Box,
+  TextField,
+  IconButton,
+  TextareaAutosize,
+  Button,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 type Props = {
@@ -6,11 +12,15 @@ type Props = {
   onMessageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onSend: () => void;
   disabled: boolean;
+  isNote: boolean;
+  handleChangeNote: () => void;
 };
 
 const MessageInput = ({
   newMessage,
   onMessageChange,
+  isNote,
+  handleChangeNote,
   onSend,
   disabled,
 }: Props) => {
@@ -19,29 +29,63 @@ const MessageInput = ({
       sx={{
         display: "flex",
         alignItems: "center",
+        flexDirection: "column",
         paddingTop: 1,
         position: "sticky",
         bottom: 0,
         backgroundColor: "background.paper",
       }}
     >
-      <TextField
-        fullWidth
-        variant="outlined"
-        size="small"
-        placeholder="Type a message..."
-        value={newMessage}
-        onChange={onMessageChange}
-        disabled={disabled}
-      />
-      <IconButton
-        color="primary"
-        onClick={onSend}
-        sx={{ ml: 1 }}
-        disabled={disabled}
+      <Box sx={{ display: "flex", alignSelf: "start", gap: 1 }}>
+        <Button
+          variant={!isNote ? "contained" : "outlined"}
+          onClick={isNote ? handleChangeNote : () => {}}
+        >
+          Reply
+        </Button>
+        <Button
+          variant={isNote ? "contained" : "outlined"}
+          onClick={!isNote ? handleChangeNote : () => {}}
+        >
+          Note
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "end",
+          flexDirection: "row",
+          paddingTop: 1,
+          width: "100%",
+          position: "sticky",
+          bottom: 0,
+          backgroundColor: "background.paper",
+        }}
       >
-        <SendIcon />
-      </IconButton>
+        <TextField
+          fullWidth
+          variant="outlined"
+          size="small"
+          placeholder="Type a message..."
+          value={newMessage}
+          onChange={onMessageChange}
+          disabled={disabled}
+          multiline
+          minRows={5}
+          maxRows={10}
+          InputProps={{
+            style: { overflow: "hidden" },
+          }}
+        />
+        <IconButton
+          color="primary"
+          onClick={onSend}
+          sx={{ ml: 1 }}
+          disabled={disabled}
+        >
+          <SendIcon />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
