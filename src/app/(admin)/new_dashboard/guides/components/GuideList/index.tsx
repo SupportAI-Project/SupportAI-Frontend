13fee5debe5053286f3e2d0af37aa3ceecb46fbd
Ori <ori100no@gmail.com>
2024-08-20 13:35:33 +0300

    added a link to the guide page from the card

--- src/app/(admin)/new_dashboard/guides/components/GuideList/index.tsx
+++ src/app/(admin)/new_dashboard/guides/components/GuideList/index.tsx
@@ -1,18 +1,32 @@
 import { Grid, Typography } from "@mui/material";
 import GuideCard from "../GuideCard";
 import { Guide } from "@/api/types/Guide";
+import { useRouter } from "next/navigation";
 
 type Props = {
-  guideItems:Guide[];
+  guideItems: Guide[];
 };
 
 const GuideList = ({ guideItems }: Props) => {
- 
+  const router = useRouter();
+
+  const handleCardClick = (guideId: number) => {
+    router.push(`guides/${guideId}`);
+  };
+
   return (
     <Grid container spacing={2}>
       {guideItems.length > 0 ? (
         guideItems.map((guide) => (
-          <Grid item xs={12} sm={6} md={4} key={guide.guideId}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            key={guide.guideId}
+            onClick={() => handleCardClick(guide.guideId)}
+            style={{ cursor: "pointer" }}
+          >
             <GuideCard guide={guide} />
           </Grid>
         ))
