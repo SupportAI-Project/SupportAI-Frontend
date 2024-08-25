fbb14883ca642ceac46a8af2f28fda5ef28b6708
Ori <ori100no@gmail.com>
2024-08-25 14:32:17 +0300

    changed to previous colors

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -2,16 +2,16 @@ import {
   createTheme,
   PaletteColorOptions,
   SimplePaletteColorOptions,
-} from '@mui/material/styles';
-import { Plus_Jakarta_Sans } from 'next/font/google';
+} from "@mui/material/styles";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
 export const plus = Plus_Jakarta_Sans({
-  weight: ['300', '400', '500', '600', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
 });
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface Palette {
     note: SimplePaletteColorOptions;
   }
@@ -21,150 +21,150 @@ declare module '@mui/material/styles' {
 }
 
 const baselightTheme = createTheme({
-  direction: 'ltr',
+  direction: "ltr",
   palette: {
     note: {
-      main: 'rgba(254, 237, 175)',
+      main: "rgba(254, 237, 175)",
     },
     primary: {
-      main: '#7091e6',
-      light: '#ede8f5',
-      dark: '#3d52a0',
+      main: "#5D87FF",
+      light: "#ECF2FF",
+      dark: "#4570EA",
     },
     secondary: {
-      main: '#49BEFF',
-      light: '#E8F7FF',
-      dark: '#456e96',
+      main: "#49BEFF",
+      light: "#E8F7FF",
+      dark: "#23afdb",
     },
     success: {
-      main: '#13DEB9',
-      light: '#E6FFFA',
-      dark: '#02b3a9',
-      contrastText: '#ffffff',
+      main: "#13DEB9",
+      light: "#E6FFFA",
+      dark: "#02b3a9",
+      contrastText: "#ffffff",
     },
     info: {
-      main: '#539BFF',
-      light: '#EBF3FE',
-      dark: '#1682d4',
-      contrastText: '#ffffff',
+      main: "#539BFF",
+      light: "#EBF3FE",
+      dark: "#1682d4",
+      contrastText: "#ffffff",
     },
     error: {
-      main: '#FA896B',
-      light: '#FDEDE8',
-      dark: '#f3704d',
-      contrastText: '#ffffff',
+      main: "#FA896B",
+      light: "#FDEDE8",
+      dark: "#f3704d",
+      contrastText: "#ffffff",
     },
     warning: {
-      main: '#FFAE1F',
-      light: '#FEF5E5',
-      dark: '#ae8e59',
-      contrastText: '#ffffff',
+      main: "#FFAE1F",
+      light: "#FEF5E5",
+      dark: "#ae8e59",
+      contrastText: "#ffffff",
     },
 
     grey: {
-      100: '#F2F6FA',
-      200: '#EAEFF4',
-      300: '#DFE5EF',
-      400: '#7C8FAC',
-      500: '#5A6A85',
-      600: '#2A3547',
+      100: "#F2F6FA",
+      200: "#EAEFF4",
+      300: "#DFE5EF",
+      400: "#7C8FAC",
+      500: "#5A6A85",
+      600: "#2A3547",
     },
     text: {
-      primary: '#2A3547',
-      secondary: '#5A6A85',
+      primary: "#2A3547",
+      secondary: "#5A6A85",
     },
     action: {
-      disabledBackground: 'rgba(73,82,88,0.12)',
+      disabledBackground: "rgba(73,82,88,0.12)",
       hoverOpacity: 0.02,
-      hover: '#f6f9fc',
+      hover: "#f6f9fc",
     },
-    divider: '#e5eaef',
+    divider: "#e5eaef",
   },
   typography: {
     fontFamily: plus.style.fontFamily,
     h1: {
       fontWeight: 600,
-      fontSize: '2.25rem',
-      lineHeight: '2.75rem',
+      fontSize: "2.25rem",
+      lineHeight: "2.75rem",
       fontFamily: plus.style.fontFamily,
     },
     h2: {
       fontWeight: 600,
-      fontSize: '1.875rem',
-      lineHeight: '2.25rem',
+      fontSize: "1.875rem",
+      lineHeight: "2.25rem",
       fontFamily: plus.style.fontFamily,
     },
     h3: {
       fontWeight: 600,
-      fontSize: '1.5rem',
-      lineHeight: '1.75rem',
+      fontSize: "1.5rem",
+      lineHeight: "1.75rem",
       fontFamily: plus.style.fontFamily,
     },
     h4: {
       fontWeight: 600,
-      fontSize: '1.3125rem',
-      lineHeight: '1.6rem',
+      fontSize: "1.3125rem",
+      lineHeight: "1.6rem",
     },
     h5: {
       fontWeight: 600,
-      fontSize: '1.125rem',
-      lineHeight: '1.6rem',
+      fontSize: "1.125rem",
+      lineHeight: "1.6rem",
     },
     h6: {
       fontWeight: 600,
-      fontSize: '1rem',
-      lineHeight: '1.2rem',
+      fontSize: "1rem",
+      lineHeight: "1.2rem",
     },
     button: {
-      textTransform: 'capitalize',
+      textTransform: "capitalize",
       fontWeight: 400,
     },
     body1: {
-      fontSize: '0.875rem',
+      fontSize: "0.875rem",
       fontWeight: 400,
-      lineHeight: '1.334rem',
+      lineHeight: "1.334rem",
     },
     body2: {
-      fontSize: '0.75rem',
-      letterSpacing: '0rem',
+      fontSize: "0.75rem",
+      letterSpacing: "0rem",
       fontWeight: 400,
-      lineHeight: '1rem',
+      lineHeight: "1rem",
     },
     subtitle1: {
-      fontSize: '0.875rem',
+      fontSize: "0.875rem",
       fontWeight: 400,
     },
     subtitle2: {
-      fontSize: '0.875rem',
+      fontSize: "0.875rem",
       fontWeight: 400,
     },
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: {
-        '*::-webkit-scrollbar': {
-          width: '8px',
+        "*::-webkit-scrollbar": {
+          width: "8px",
         },
-        '*::-webkit-scrollbar-track': {
-          backgroundColor: '#EEF0E5',
+        "*::-webkit-scrollbar-track": {
+          backgroundColor: "transparent",
         },
-        '*::-webkit-scrollbar-thumb': {
-          backgroundColor: '#C4C4C4',
-          borderRadius: '8px',
+        "*::-webkit-scrollbar-thumb": {
+          backgroundColor: "#C4C4C4",
+          borderRadius: "8px",
         },
-        '*::-webkit-scrollbar-thumb:hover': {
-          backgroundColor: '#A0A0A0',
+        "*::-webkit-scrollbar-thumb:hover": {
+          backgroundColor: "#A0A0A0",
         },
-        '.MuiPaper-elevation9, .MuiPopover-root .MuiPaper-elevation': {
+        ".MuiPaper-elevation9, .MuiPopover-root .MuiPaper-elevation": {
           boxShadow:
-            'rgb(145 158 171 / 30%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px !important',
+            "rgb(145 158 171 / 30%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px !important",
         },
       },
     },
     MuiCard: {
       styleOverrides: {
         root: {
-          borderRadius: '7px',
+          borderRadius: "7px",
         },
       },
     },
