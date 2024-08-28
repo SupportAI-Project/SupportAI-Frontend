17ab60da2fd468a9d02288059ee3ff83e688faf8
Ori <ori100no@gmail.com>
2024-08-28 23:00:32 +0300

    added title to input when adding review

--- src/app/(admin)/new_dashboard/guides/hooks/useAddReviewForm.ts
+++ src/app/(admin)/new_dashboard/guides/hooks/useAddReviewForm.ts
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import { CreateReviewDto, CreateReviewSchema } from '../dto/CreateReviewDto';
-import { useAddReview } from '@/hooks';
+import { useState } from "react";
+import { CreateReviewDto, CreateReviewSchema } from "../dto/CreateReviewDto";
+import { useAddReview } from "@/hooks";
 
 const useAddReviewForm = (guideId: number) => {
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
+  const [title, setTitle] = useState("");
   const [stars, setStars] = useState(1);
   const [validationError, setValidationError] = useState<string | null>(null);
   const { mutate, isError, error, isPending, isSuccess } = useAddReview();
@@ -12,19 +13,21 @@ const useAddReviewForm = (guideId: number) => {
     const newReview: CreateReviewDto = {
       guideId,
       rating: stars ?? 0,
-      comment: comment.trim() || '',
+      title: title.trim(),
+      comment: comment.trim() || "",
     };
     const validation = CreateReviewSchema.safeParse(newReview);
     if (!validation.success) {
       setValidationError(
-        validation.error.errors.map((err) => err.message).join(', ')
+        validation.error.errors.map((err) => err.message).join(", ")
       );
       return;
     }
 
     mutate(validation.data, {
       onSuccess: () => {
-        setComment('');
+        setComment("");
+        setTitle("");
         setStars(0);
       },
     });
@@ -33,6 +36,8 @@ const useAddReviewForm = (guideId: number) => {
   return {
     comment,
     setComment,
+    title,
+    setTitle,
     stars,
     setStars,
     validationError,
