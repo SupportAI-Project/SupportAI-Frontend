53e1d245b252e088bed5474647e8e6fcb725865a
Ori <ori100no@gmail.com>
2024-08-28 22:20:33 +0300

    lint

--- src/api/types/Guide.ts
+++ src/api/types/Guide.ts
@@ -1,6 +1,6 @@
-import { z } from 'zod';
-import { userSchema } from './User';
-import { reviewSchema } from './Review';
+import { z } from "zod";
+import { userSchema } from "./User";
+import { reviewSchema } from "./Review";
 
 const guideSchema = z.object({
   id: z.number(),
