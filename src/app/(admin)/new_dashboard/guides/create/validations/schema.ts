f249160496c71eff7981181f85a4a3cdd436efec
Ori <ori100no@gmail.com>
2024-08-28 12:03:00 +0300

    changed schema to fit new guide type

--- src/app/(admin)/new_dashboard/guides/create/validations/schema.ts
+++ src/app/(admin)/new_dashboard/guides/create/validations/schema.ts
@@ -1,6 +1,8 @@
+import { getEnabledCategories } from "trace_events";
 import { z } from "zod";
 
 export const schema = z.object({
   contentHTML: z.string(),
   title: z.string(),
+  categories: z.array(z.string()).max(3),
 });
