81a3fc20f0f1176893fb9151608b13a1c445f2f1
Tomer <tomermi123@gmail.com>
2024-08-31 14:43:47 +0300

    feat: Refactor useSearchGuides hook to include sorting functionality

--- src/app/(admin)/new_dashboard/guides/hooks/useSearchGuides.ts
+++ src/app/(admin)/new_dashboard/guides/hooks/useSearchGuides.ts
@@ -1,29 +1,25 @@
 import { useState } from "react";
 import { Guide } from "@/api/types/Guide";
 import { SelectChangeEvent } from "@mui/material";
+import { SortCriteria } from "../types";
+import { sortByCriteria } from "./sortGuides";
 
 export const useSearchGuides = (initialGuides: Guide[]) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedTag, setSelectedTag] = useState("All");
-  const [sortCriteria, setSortCriteria] = useState<"rating" | "date">("rating");
+  const [sortCriteria, setSortCriteria] = useState<SortCriteria>("rating");
 
-  const filteredGuides = initialGuides.filter((guide) => {
-    const matchesQuery = guide.title
-      .toLowerCase()
-      .includes(searchQuery.toLowerCase());
-    const matchesTag =
-      selectedTag === "All" || guide.categories.includes(selectedTag);
-    return matchesQuery && matchesTag;
-  })
-    .sort((a, b) => {
-      if (sortCriteria === "rating") {
-        const avgRatingA = a.reviews?.reduce((acc, review) => acc + review.rating, 0) ?? 1;
-        const avgRatingB = b.reviews?.reduce((acc, review) => acc + review.rating, 0) ?? 1;
-        return avgRatingB - avgRatingA;
-      } else {
-        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-      }
-    });
+  const filteredGuides = sortByCriteria(
+    initialGuides.filter((guide) => {
+      const matchesQuery = guide.title
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase());
+      const matchesTag =
+        selectedTag === "All" || guide.categories.includes(selectedTag);
+      return matchesQuery && matchesTag;
+    }),
+    sortCriteria
+  );
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
@@ -34,7 +30,7 @@ export const useSearchGuides = (initialGuides: Guide[]) => {
   };
 
   const handleSortChange = (event: SelectChangeEvent<string> ) => {
-    setSortCriteria(event.target.value as "rating" | "date");
+    setSortCriteria(event.target.value as SortCriteria);
   };
 
   return {
