daa4ed4cb449a5f1d16ca90693f973018d94e142
Tomer <tomermi123@gmail.com>
2024-08-31 12:17:47 +0300

    Feat: Refactor useSearchGuides hook to include sorting functionality

--- src/app/(admin)/new_dashboard/guides/hooks/useSearchGuides.ts
+++ src/app/(admin)/new_dashboard/guides/hooks/useSearchGuides.ts
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Guide } from "@/api/types/Guide";
+import { SelectChangeEvent } from "@mui/material";
 
 export const useSearchGuides = (initialGuides: Guide[]) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedTag, setSelectedTag] = useState("All");
+  const [sortCriteria, setSortCriteria] = useState<"rating" | "date">("rating");
 
   const filteredGuides = initialGuides.filter((guide) => {
     const matchesQuery = guide.title
@@ -12,7 +14,16 @@ export const useSearchGuides = (initialGuides: Guide[]) => {
     const matchesTag =
       selectedTag === "All" || guide.categories.includes(selectedTag);
     return matchesQuery && matchesTag;
-  });
+  })
+    .sort((a, b) => {
+      if (sortCriteria === "rating") {
+        const avgRatingA = a.reviews?.reduce((acc, review) => acc + review.rating, 0) ?? 1;
+        const avgRatingB = b.reviews?.reduce((acc, review) => acc + review.rating, 0) ?? 1;
+        return avgRatingB - avgRatingA;
+      } else {
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+      }
+    });
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
@@ -22,11 +33,17 @@ export const useSearchGuides = (initialGuides: Guide[]) => {
     setSelectedTag(value || "All");
   };
 
+  const handleSortChange = (event: SelectChangeEvent<string> ) => {
+    setSortCriteria(event.target.value as "rating" | "date");
+  };
+
   return {
     searchQuery,
     selectedTag,
+    sortCriteria,
     filteredGuides,
     handleSearchChange,
     handleTagChange,
+    handleSortChange,
   };
 };
