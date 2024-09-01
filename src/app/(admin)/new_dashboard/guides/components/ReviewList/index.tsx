2d9102a15aaefa3212d49ca875a21479d4ecb2be
Ori <ori100no@gmail.com>
2024-09-01 12:53:14 +0300

    fixed review box style

--- src/app/(admin)/new_dashboard/guides/components/ReviewList/index.tsx
+++ src/app/(admin)/new_dashboard/guides/components/ReviewList/index.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import {
-  Typography,
-  Box,
-  Button,
-  Divider,
-} from "@mui/material";
+import { Typography, Box, Button, Divider } from "@mui/material";
 import { Review } from "@/api/types/Review";
 import AddReviewBox from "../AddReviewBox";
 import ReviewCard from "../ReviewCard";
@@ -18,45 +13,46 @@ interface ReviewListProps {
 }
 
 const ReviewList: React.FC<ReviewListProps> = ({ reviews, guideId }) => {
-  const [visibleCount, setVisibleCount] = useState(1); 
+  const [visibleCount, setVisibleCount] = useState(1);
 
   const showMoreReviews = () => {
-    setVisibleCount((prevCount) => Math.min(prevCount + 2, reviews.length)); 
+    setVisibleCount((prevCount) => Math.min(prevCount + 2, reviews.length));
   };
 
   const showLessReviews = () => {
     setVisibleCount(1);
-  }
+  };
 
   let reviewsContext = !reviews.length ? (
     <Typography>No reviews available yet</Typography>
   ) : (
-    reviews.slice(0, visibleCount).map((review, index) => (
-        <ReviewCard key={index} review={review} />
-    ))
+    reviews
+      .slice(0, visibleCount)
+      .map((review, index) => <ReviewCard key={index} review={review} />)
   );
 
   return (
-
     <Box mt={3}>
-      <DashboardCard title={"Reviews"}> 
+      <DashboardCard title={"Reviews"}>
         <Box>
-          <Box sx={{border:"1px solid" , borderColor:"border.main", padding:2, borderRadius:3}}>
-            {reviewsContext}
-          </Box>
-          {reviews?.length > 1 &&(
+          <Box sx={{ padding: 2, borderRadius: 3 }}>{reviewsContext}</Box>
+          {reviews?.length > 1 && (
             <Box display="flex" justifyContent="center" width="100%">
-              <Button onClick={()=>{
-                visibleCount === reviews.length ? showLessReviews() : showMoreReviews();
-              }}>
+              <Button
+                onClick={() => {
+                  visibleCount === reviews.length
+                    ? showLessReviews()
+                    : showMoreReviews();
+                }}
+              >
                 {visibleCount === reviews.length ? "Show Less" : "Show More"}
               </Button>
-            </Box>)}
+            </Box>
+          )}
         </Box>
       </DashboardCard>
       <AddReviewBox guideId={guideId ?? 0} />
-  </Box>
-  
+    </Box>
   );
 };
 
